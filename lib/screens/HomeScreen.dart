aae9ac0f3d36a0e91b429adfaf7a5dd6134696e3
Sang <sang.nguyen2000@hotmail.de>
2022-03-15 11:23:05 +0100

    - project set up - added bottom naviagtion bar

---
+++ lib/screens/HomeScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final ThemeData themeData = Theme.of(context);
+    const double padding = 25;
+    const sidePadding = EdgeInsets.symmetric(horizontal: 25);
+
+    return Container(
+      child: Text("Home"),
+    );
+  }
+}
