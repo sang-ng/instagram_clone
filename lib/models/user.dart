65b09fc66f6b11340a64f4035d1d18c29b397ac2
Sang <sang.nguyen2000@hotmail.de>
2022-03-16 09:33:19 +0100

    - added sample data

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,7 +2,6 @@ class User {
   final String name;
   final String username;
   final String image;
-  bool isFollowedByMe;
 
-  User(this.name, this.username, this.image, this.isFollowedByMe);
+  User(this.name, this.username, this.image);
 }
